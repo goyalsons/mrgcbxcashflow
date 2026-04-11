b87c1f716cd1d99b6125b1e112d0e059a025070d
Sanjeev Jain <sj@sanjeevjain.com>
2026-04-11 10:41:48 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -11,7 +11,7 @@ export default function AppLayout({ user }) {
       <Sidebar user={user} collapsed={collapsed} onToggle={() => setCollapsed(!collapsed)} />
       <div className={`flex-1 flex flex-col min-h-screen transition-all duration-300 ${collapsed ? 'ml-[68px]' : 'ml-60'}`}>
         <TopBar user={user} />
-        <main className="flex-1 p-5 md:p-7">
+        <main className="flex-1 p-5 md:p-7 overflow-x-hidden">
           <div className="max-w-[1440px] mx-auto">
             <Outlet />
           </div>
